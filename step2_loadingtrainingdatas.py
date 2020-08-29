fc55fdd079db3f23b1961195435ec5f971ce421c
Ismael Miled <57010036+imiled@users.noreply.github.com>
2020-08-29 18:19:20 +0200

    Update and rename step2_loadingexistingdatas.py to step2_loadingtrainingdatas.py

--- step2_loadingtrainingdatas.py
+++ step2_loadingtrainingdatas.py
@@ -40,27 +40,16 @@ X_train_image=pd.read_csv('datas/X_train_image.csv')
 Y_train_StateClass_image=pd.read_csv('datas/Y_train_StateClass_image.csv')
 Y_train_FutPredict_image=pd.read_csv('datas/Y_train_FutPredict_image.csv')
 
-X_test_image=pd.read_csv('datas/X_test_image.csv')
-Y_test_StateClass_image=pd.read_csv('datas/Y_test_StateClass_image.csv')
-Y_test_FutPredict_image=pd.read_csv('datas/Y_test_FutPredict_image.csv')
 
 #setting up the index to Date
 X_train_image=X_train_image.set_index("Date")
 Y_train_StateClass_image=Y_train_StateClass_image.set_index("Date")
 Y_train_FutPredict_image=Y_train_FutPredict_image.set_index("Date")
 
-X_test_image=X_test_image.set_index("Date")
-Y_test_StateClass_image=Y_test_StateClass_image.set_index("Date")
-Y_test_FutPredict_image=Y_test_FutPredict_image.set_index("Date")
-
 #modify dataset to np array for input to NN
-x_test=change_X_df__nparray_image(X_train_image)
+x_train=change_X_df__nparray_image(X_train_image)
 y_train_state=np.array(Y_train_StateClass_image)
 y_train_value=np.array(Y_train_FutPredict_image)
-
-x_test=change_X_df__nparray_image(X_test_image)
-y_test_state=np.array(Y_train_StateClass_image)
-y_test_value=np.array(Y_train_FutPredict_image)
 
 ##Setting up xtrain and ytrain
 #Here we focus on predicting the future state Y_train_StateClass_image
@@ -74,14 +63,14 @@ for i in range(nb_train):
 y_train=np.array(Y_train_StateClass_image)
 #y_train=np.array(Y_train_FutPredict_image)
 
-nb_test=len(X_test_image.index)
-x_test=np.zeros((nb_test,32,32,3))
-for i in range(nb_test):
-  tmp=np.array(X_test_image.iloc[i])
+nb_train=len(X_train_image.index)
+x_train=np.zeros((nb_train,32,32,3))
+for i in range(nb_train):
+  tmp=np.array(X_train_image.iloc[i])
   tmp=tmp.reshape(32,32,3)
-  x_test[i]=tmp
+  x_train[i]=tmp
 
-y_test=np.array(Y_test_StateClass_image)
+y_train=np.array(Y_train_StateClass_image)
 #y_test=np.array(Y_test_FutPredict_image)
 
 
