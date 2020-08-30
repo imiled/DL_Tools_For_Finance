ce1f49a53afee3e2beee1467568f42d75a6607ae
Ismael Miled <57010036+imiled@users.noreply.github.com>
2020-08-30 15:07:29 +0200

    Update step5_guess_future_marketstate_from_image.py

--- step5_guess_future_marketstate_from_image.py
+++ step5_guess_future_marketstate_from_image.py
@@ -8,21 +8,23 @@ trained_model_path='model/vggforsp500.h5'
 image_path='ImageM/image1.PNG'
 
 #Load the image and resize it to 32x32 and taking off the transparency
-load_img_rz = np.array(Image.open('image_path').resize((32,32)))
+load_img_rz = np.array(Image.open(image_path).resize((32,32)))
 #Image.fromarray(load_img_rz).save('/content/drive/My Drive/_sample_data/ImageM/image1.PNG')
 image=load_img_rz[:,:,:3]/255
-print("After resizing:",load_img_rz.shape)
+print("After resizing:",image.shape)
 
+#petite astuce pour ne pas avoir d erreur avec les types list, tensors,  nparray et dataframe
+doubleimage=np.array([image,image])
 ############
 #recuperation of the model
 vggsp500model = load_model(trained_model_path)
-Y_pred = vggsp500model.predict(x_test)
+Y_pred = vggsp500model.predict(doubleimage)
 y_pred = np.argmax(Y_pred, axis=1)
 
-target_state = ['SS', 'SN', 'N','NB','BB']
+target_state = ['SS', 'SN', 'N','NB','BB','Error']
 df_result=pd.DataFrame((Y_pred))
 
 df_result.columns=target_state
 df_result.index=[image_path, image_path+'1']
-print(df_result)
 print ("for ",image_path, "the best result is ", target_state[int(y_pred[0])] )
+df_result
